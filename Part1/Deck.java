64a2eac03702661432761469ac4f38fb884eb581
MUHAMMAD ALIF BIN KHABALI <1211103373@student.mmu.edu.my>
2023-05-05 14:26:59 +0800

    k

--- Part1/Deck.java
+++ Part1/Deck.java
@@ -10,7 +10,7 @@ public class Deck {
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K","A"};
         for (String suit : suits) {
             for (String rank : ranks) {
-                this.cards.add(suit + rank );
+                this.cards.add(suit + rank ); 
             }
         }
     }
