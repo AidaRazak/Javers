80f1a8ea9c31332840a5bbbb060a9d30f50a6819
arshadhanisham <112753836+arshadhanisham@users.noreply.github.com>
2023-06-11 12:03:37 +0800

    Add files via upload

--- PART 2/Deck.java
+++ PART 2/Deck.java
@@ -3,53 +3,71 @@ import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-    private List<Card> cards;
+    private List<String> cards;
+    private List<String> centerCards;
 
     public Deck() {
+        initializeDeck();
+        centerCards = new ArrayList<>();
+        centerCards.add(cards.remove(cards.size() - 1));
+    }
+
+    public void updateCenterCards(String card) {
+        centerCards.add(card);
+    }
+
+   
+    private void initializeDeck() {
         cards = new ArrayList<>();
-        String[] suits = {"h", "d", "c", "s"};
-        String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "X", "J", "Q", "K"};
+        String[] ranks = {"A", "5", "9", "K", "2", "6", "X", "3", "7", "J", "4", "8", "Q"};
+        String[] suits = {"s", "h", "d", "c"};
+
         for (String suit : suits) {
             for (String rank : ranks) {
-                cards.add(new Card(suit, rank));
+                cards.add(rank + suit);
             }
         }
-        shuffle();
-    }
 
-    public boolean isEmpty() {
-        return cards.isEmpty();
-    }
-
-    public Card drawCard() {
-        if (isEmpty()) {
-            return null; // or throw an exception indicating an empty deck
-        }
-        return cards.remove(cards.size() - 1);
-    }
-
-    public void resetDeck() {
-        cards.clear();
-        String[] suits = {"H", "D", "C", "S"};
-        String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "X", "J", "Q", "K"};
-        for (String suit : suits) {
-            for (String rank : ranks) {
-                cards.add(new Card(suit, rank));
-            }
-        }
-        shuffle();
-    }
-
-    public void shuffle() {
         Collections.shuffle(cards);
     }
 
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (Card card : cards) {
-            sb.append(card.toString()).append(" ");
+    public List<List<String>> dealToPlayers(int numPlayers, int numCards) {
+        List<List<String>> playerHands = new ArrayList<>();
+
+        for (int i = 0; i < numPlayers; i++) {
+            List<String> hand = new ArrayList<>();
+            for (int j = 0; j < numCards; j++) {
+                hand.add(cards.remove(cards.size() - 1));
+            }
+            playerHands.add(hand);
         }
-        return sb.toString();
+
+        return playerHands;
+    }
+
+    public List<String> getCenterCards() {
+        return centerCards;
+    }
+    
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
+    
+    public String drawCard() {
+        if (!cards.isEmpty()) {
+            return cards.remove(cards.size() - 1);
+        } else {
+            throw new IllegalStateException("The deck is empty.");
+        }
+    }
+
+   
+    
+
+    @Override
+    public String toString() {
+        return "Deck{" +
+                "cards=" + cards +
+                '}';
     }
 }
-
