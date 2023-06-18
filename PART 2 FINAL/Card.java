4a40ca3586ca298848ce3e4ab04da85219ecefa9
sofeaHazreenahasdi <132485282+sofeaHazreenahasdi@users.noreply.github.com>
2023-06-18 16:08:45 +0800

    Update Card.java

--- PART 2 FINAL/Card.java
+++ PART 2 FINAL/Card.java
@@ -21,11 +21,11 @@ public class Card implements Serializable {
 
     public static int compareRanks(Card card1, Card card2) {
         Map<String, Integer> rankToValue = new LinkedHashMap<>();
-        rankToValue.put("A", 14); // Make Ace the highest
+        rankToValue.put("A", 14); 
         rankToValue.put("K", 13);
         rankToValue.put("Q", 12);
         rankToValue.put("J", 11);
-        rankToValue.put("X", 10); // Assuming X is a ten
+        rankToValue.put("X", 10); 
         rankToValue.put("9", 9);
         rankToValue.put("8", 8);
         rankToValue.put("7", 7);
@@ -38,8 +38,7 @@ public class Card implements Serializable {
         int rankValue1 = rankToValue.get(card1.getRank());
         int rankValue2 = rankToValue.get(card2.getRank());
 
-        // Note that the order of parameters in the compare method is switched
-        // to make it so that a higher rank returns a positive value
+        
         return Integer.compare(rankValue1, rankValue2);
     }
 
